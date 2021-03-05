cc77d72bc87f5de183880896e9a51b4f9f7f116e
itstoasted-0 <snow.w686@gmail.com>
2021-03-05 22:58:13 +0900

    ログインの保持機能

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,5 @@
 module SessionsHelper
+
   def log_in(user)
     session[:user_id] = user.id
   end
@@ -19,7 +20,7 @@ module SessionsHelper
     @current_user = nil
   end
 
-# ユーザーのセッションを永続的にする
+  # ユーザーのセッションを永続的にする
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
@@ -32,12 +33,13 @@ module SessionsHelper
       @current_user ||= User.find_by(id: session[:user_id])
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
+      elsif (user_id = cookies.signed[:user_id])
+        user = User.find_by(id: user_id)
+        if user && user.authenticated?(cookies[:remember_token])
+          log_in user
+          @current_user = user
+        end
+    end
     end
   end
 
@@ -47,5 +49,6 @@ module SessionsHelper
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
+
 
 end
