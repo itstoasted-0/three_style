18fd5c9854132b3f2ae47165b7bdc2d97b122927
itstoasted-0 <snow.w686@gmail.com>
2021-03-14 09:34:38 +0900

    投稿の編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user
+  before_action :correct_user, only: [:edit, :update]
+
   def index
     @posts = Post.order(:id)
   end
@@ -21,10 +24,30 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:success] = "更新されました！"
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
 
   private
 
   def post_params
     params.require(:post).permit(:genre, :content)
   end
+
+  def correct_user
+      # 現在のユーザーが更新対象の投稿を保有しているかどうか確認
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
+    end
 end
