153d8293dba2f11f9179da6ac10f7c455578b5ee
itstoasted-0 <snow.w686@gmail.com>
2021-03-17 15:04:33 +0900

    受動的なフォロー

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
 
   class << self
     # 渡された文字列のハッシュ値を返す
@@ -61,6 +66,11 @@ class User < ApplicationRecord
   # 現在のユーザーがフォローしてたらtrueを返す
   def following?(other_user)
     following.include?(other_user)
+  end
+
+  # 現在のユーザーがフォローされていたらtrueを返す
+  def followed_by?(other_user)
+    followers.include?(other_user)
   end
 
   private
