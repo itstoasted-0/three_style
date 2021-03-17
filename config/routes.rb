c954af210c68b6244e2146e9501eddb5c8d6aeaf
itstoasted-0 <snow.w686@gmail.com>
2021-03-17 21:08:34 +0900

    Favoritesコントローラ、ルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
- root 'static_pages#home'
+ 
+  root 'static_pages#home'
+ 
  get :about,        to: 'static_pages#about'
  get :use_of_terms, to: 'static_pages#terms'
  get :signup,       to: 'users#new'
+
  resources :users do
    member do
      get :following, :followers
@@ -11,6 +14,11 @@ Rails.application.routes.draw do
  get  :login,        to: 'sessions#new'
  post :login,        to: 'sessions#create'
  delete :logout,     to: 'sessions#destroy'
+ 
  resources :posts
+ 
  resources :relationships, only: [:create, :destroy]
+
+ post   "favorites/:post_id/create"  => "favorites#create"
+ delete "favorites/:post_id/destroy" => "favorites#destroy"
 end
