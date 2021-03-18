b179c1851d9c60d6ca4275f775b642dd330b9c86
itstoasted-0 <snow.w686@gmail.com>
2021-03-18 20:14:54 +0900

    お気に入り一覧ページ

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,10 @@
 class FavoritesController < ApplicationController
   before_action :logged_in_user
 
+  def index
+    @favorites = current_user.favorites
+  end
+
   def create
     @post = Post.find(params[:post_id])
     @user = @post.user
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,24 @@
 Rails.application.routes.draw do
- 
   root 'static_pages#home'
- 
- get :about,        to: 'static_pages#about'
- get :use_of_terms, to: 'static_pages#terms'
- get :signup,       to: 'users#new'
+  
+  get :about,        to: 'static_pages#about'
+  get :use_of_terms, to: 'static_pages#terms'
+  get :signup,       to: 'users#new'
 
- resources :users do
-   member do
-     get :following, :followers
-   end
- end
- get  :login,        to: 'sessions#new'
- post :login,        to: 'sessions#create'
- delete :logout,     to: 'sessions#destroy'
- 
- resources :posts
- 
- resources :relationships, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  get  :login,        to: 'sessions#new'
+  post :login,        to: 'sessions#create'
+  delete :logout,     to: 'sessions#destroy'
+  
+  resources :posts
+  
+  resources :relationships, only: [:create, :destroy]
 
- post   "favorites/:post_id/create"  => "favorites#create"
- delete "favorites/:post_id/destroy" => "favorites#destroy"
+  get :favorites, to: 'favorites#index'
+  post   "favorites/:post_id/create"  => "favorites#create"
+  delete "favorites/:post_id/destroy" => "favorites#destroy"
 end
