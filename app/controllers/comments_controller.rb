89257f77a0475ca7da8b98251db46a77b8cf5694
itstoasted-0 <snow.w686@gmail.com>
2021-03-19 00:01:50 +0900

    通知を生成する

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,13 @@ class CommentsController < ApplicationController
     @comment = @post.comments.build(user_id: current_user.id, content: params[:comment][:content])
     if !@post.nil? && @comment.save
       flash[:success] = "コメントを追加しました！"
+      # 自分以外のユーザーからコメントがあったときのみ通知を作成
+      if @user != current_user
+        @user.notifications.create(post_id: @post.id, variety: 2,
+                                   from_user_id: current_user.id,
+                                   content: @comment.content) # コメントは通知種別2
+        @user.update_attribute(:notification, true)
+      end
     else
       flash[:danger] = "空のコメントは投稿できません。"
     end
