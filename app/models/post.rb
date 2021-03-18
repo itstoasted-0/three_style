4d54f1592514640091f596fc344e085c04daf9a0
itstoasted-0 <snow.w686@gmail.com>
2021-03-18 20:29:01 +0900

    Commentモデルの作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
   validates :genre, presence: true
