741a371be09671faab116fa603d49d6d7dc3fb90
itstoasted-0 <snow.w686@gmail.com>
2021-03-29 00:25:37 +0900

    edit_header

--- config/routes.rb
+++ config/routes.rb
@@ -26,9 +26,9 @@ Rails.application.routes.draw do
 
   resources :notifications, only: :index
 
-  devise_for :users
-  devise_scope :user do
-    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
-  end
+  # devise_for :users
+  # devise_scope :user do
+  #   post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  # end
 
 end
